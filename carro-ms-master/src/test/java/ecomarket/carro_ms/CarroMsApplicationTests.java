7cf58ff935a98c03e2074e0cc27f96e55f0454a2
PedroSuarez91 <pe.suarez@duocuc.cl>
2026-06-27 19:15:23 -0400

    cambios

--- carro-ms-master/src/test/java/ecomarket/carro_ms/CarroMsApplicationTests.java
+++ carro-ms-master/src/test/java/ecomarket/carro_ms/CarroMsApplicationTests.java
@@ -1,5 +1,7 @@
 package ecomarket.carro_ms;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class CarroMsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void mainClassExists() {
+		assertNotNull(CarroMsApplication.class);
 	}
 
 }
