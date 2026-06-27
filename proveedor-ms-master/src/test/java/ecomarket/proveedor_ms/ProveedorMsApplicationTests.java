4efeae0cbbb4e2f7f5ea0693e0196213ecfec9e3
PedroSuarez91 <pe.suarez@duocuc.cl>
2026-06-27 18:24:22 -0400

    cambios

--- proveedor-ms-master/src/test/java/ecomarket/proveedor_ms/ProveedorMsApplicationTests.java
+++ proveedor-ms-master/src/test/java/ecomarket/proveedor_ms/ProveedorMsApplicationTests.java
@@ -1,5 +1,7 @@
 package ecomarket.proveedor_ms;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class ProveedorMsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void mainClassExists() {
+		assertNotNull(ProveedorMsApplication.class);
 	}
 
 }
