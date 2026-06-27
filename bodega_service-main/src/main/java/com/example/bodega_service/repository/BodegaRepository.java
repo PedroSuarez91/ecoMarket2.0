280daaa9d8126f411343ef43966667ed22df8d91
PedroSuarez91 <pe.suarez@duocuc.cl>
2026-06-27 18:01:56 -0400

    cambios

--- bodega_service-main/src/main/java/com/example/bodega_service/repository/BodegaRepository.java
+++ bodega_service-main/src/main/java/com/example/bodega_service/repository/BodegaRepository.java
@@ -1,7 +1,7 @@
 package com.example.bodega_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.example.bodega_service.model.Bodega;
 
--- catalogo-main/src/main/java/ecomarket/catalogo/repository/ProductoRepository.java
+++ catalogo-main/src/main/java/ecomarket/catalogo/repository/ProductoRepository.java
@@ -1,7 +1,7 @@
 package ecomarket.catalogo.repository;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
